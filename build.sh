90fb3b0349cac904eb630486f9f430385626fc37
simplesteph <stephane.maarek@gmail.com>
2016-12-14 16:10:10 +1100

    one environment file to source

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 # You shouldn't have to run this file
 # This describes how Docker Hub will build the images based on tags
-
-ANSIBLE_BRANCH=devel
-ANSIBLE_VERSION=2.3.0
-ANSIBLE_COMMIT_HASH=f28b5a0ed86d9b007744b7815cb2ca4a283275f4
-DOCKER_TAG=$ANSIBLE_BRANCH-$ANSIBLE_VERSION-$ANSIBLE_COMMIT_HASH
+source ANSIBLE_DOCKER_ENV
 
 docker build . -t simplemachines/ansible-template:$DOCKER_TAG
--- run-playbook.sh
+++ run-playbook.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-ANSIBLE_BRANCH=devel
-ANSIBLE_VERSION=2.3.0
-ANSIBLE_COMMIT_HASH=f28b5a0ed86d9b007744b7815cb2ca4a283275f4
-DOCKER_TAG=$ANSIBLE_BRANCH-$ANSIBLE_VERSION-$ANSIBLE_COMMIT_HASH
+source ANSIBLE_DOCKER_ENV
 
 docker run -it -v "$(pwd):/project" -v ~/.aws:/root/.aws -e DEPLOY_CONFIG=$1 simplemachines/ansible-template:$DOCKER_TAG scripts/run-playbook.sh
