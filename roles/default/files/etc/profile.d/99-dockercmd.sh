bfa08d6acf364345cc0bbf7cf26dda49cc6e5108
simplesteph <stephane.maarek@gmail.com>
2017-01-03 12:28:29 +1100

    refactored some code to have sh scripts as files

---
+++ roles/default/files/etc/profile.d/99-dockercmd.sh
@@ -0,0 +1,6 @@
+alias dps='docker ps'
+function dl { (docker ps | fgrep 'aws.com/kafka:' | awk '{print $1}'; docker ps -l -q) | head -1;}
+function dlog { x="$(dl)"; if [ -z "$x" ]; then echo "Container not running."; else docker logs "$@" $x; fi ;}
+alias dlogf='dlog -f'
+alias dlogt='dlog -t'
+function dex { x="$(dl)"; if [ -z "$x" ]; then echo "Container not running."; else docker exec -it $x "$@"; fi ;}
